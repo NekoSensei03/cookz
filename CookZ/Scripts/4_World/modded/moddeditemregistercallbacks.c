50ad65536e9b83439889c17f09172ed54ee35028
pp <pp@example.com>
2024-09-01 14:48:36 +0200

    add new textures for boxes, cans and the cookbook fix hand position of cookbook

--- CookZ/Scripts/4_World/modded/moddeditemregistercallbacks.c
+++ CookZ/Scripts/4_World/modded/moddeditemregistercallbacks.c
@@ -6,5 +6,7 @@ modded class ModItemRegisterCallbacks
         pType.AddItemInHandsProfileIK("CookZ_EmptyBox",			"dz/anims/workspaces/player/player_main/player_main_1h.asi",	pBehavior,	"dz/anims/anm/player/ik/gear/pepper_green_fresh.anm");
         pType.AddItemInHandsProfileIK("CookZ_Box_Base",			"dz/anims/workspaces/player/player_main/player_main_1h.asi",	pBehavior,	"dz/anims/anm/player/ik/gear/pepper_green_fresh.anm");
         pType.AddItemInHandsProfileIK("CookZ_BoxOpened_Base",	"dz/anims/workspaces/player/player_main/player_main_1h.asi",	pBehavior,	"dz/anims/anm/player/ik/gear/pepper_green_fresh.anm");
+        
+        pType.AddItemInHandsProfileIK("CookZ_Cookbook_Item",	"dz/anims/workspaces/player/player_main/player_main_1h.asi",	pBehavior,	"dz/anims/anm/player/ik/gear/book.anm");
    	};
 };
